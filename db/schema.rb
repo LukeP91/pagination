a7ba77d14489c335d16c848c2c35d253a68b9cff
Luke Pawlik <lukepawlik@gmail.com>
2016-11-22 22:17:50 +0100

    add migration for Categories.
    * Category can have name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161122220650) do
+ActiveRecord::Schema.define(version: 20161122221450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 20161122220650) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
   end
 
 end
